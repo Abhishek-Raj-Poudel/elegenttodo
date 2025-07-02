73be128f8eb8a804af980f63420cf6d961a33b18
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-07-02 21:43:40 +0545

    Feat: Generated MRC for Todo Tags and Todotags

---
+++ app/Http/Controllers/TodoTagController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TodoTag;
+use Illuminate\Http\Request;
+
+class TodoTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TodoTag $todoTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TodoTag $todoTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TodoTag $todoTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TodoTag $todoTag)
+    {
+        //
+    }
+}
